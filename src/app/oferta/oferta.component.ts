f33f29fcf118de4b144904581ea2cf484e85d817
Roneison M de Freitas <roneison.machado@gmail.com>
2020-01-06 15:06:49 -0300

    125. Recuperando a oferta selecionada (HTTP Request/API Rest/Promise)

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -1,20 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { OfertasService } from '../ofertas.service';
+import { Oferta } from '../shared/oferta.model';
 
 @Component({
   selector: 'app-oferta',
   templateUrl: './oferta.component.html',
-  styleUrls: ['./oferta.component.css']
+  styleUrls: ['./oferta.component.css'],
+  providers: [OfertasService]
 })
 export class OfertaComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private ofertaService: OfertasService
+  ) { }
 
   ngOnInit() {
-    //console.log("ID: ", this.route.snapshot.params['id'])
-    this.route.params.subscribe((param: any) => {
-      console.log(param.id);
+    this.ofertaService.getOfertasById(this.route.snapshot.params['id'])
+      .then((oferta: Oferta) => {
+        console.log(oferta)
     })
+    /*this.route.params.subscribe((param: any) => {
+      console.log(param.id);
+    })*/
   }
 
 }
