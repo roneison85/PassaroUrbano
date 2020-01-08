7b063037e0c68c87783441f42cbddcaccd9e87fc
Roneison M de Freitas <roneison.machado@gmail.com>
2020-01-08 13:33:23 -0300

    134 e 135. Recuperando dados 'Como usar' e 'Onde Fica' via HTTP Request

--- src/app/oferta/como-usar/como-usar.component.ts
+++ src/app/oferta/como-usar/como-usar.component.ts
@@ -1,17 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { OfertasService } from 'src/app/ofertas.service';
 
 @Component({
   selector: 'app-como-usar',
   templateUrl: './como-usar.component.html',
-  styleUrls: ['./como-usar.component.css']
+  styleUrls: ['./como-usar.component.css'],
+  providers: [ OfertasService ]
 })
 export class ComoUsarComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) { }
+  public descricao: string = '';
+
+  constructor(
+    private route: ActivatedRoute, 
+    private ofertasService: OfertasService) { }
 
   ngOnInit() {
-    console.log('ID da rota pai: ', this.route.parent.snapshot.params['id']);
+    this.ofertasService.getComoUsarOfertaPorId(this.route.parent.snapshot.params['id'])
+      .then((descricao: string) => {
+        this.descricao = descricao;
+      })
   }
 
 }
--- src/app/oferta/onde-fica/onde-fica.component.ts
+++ src/app/oferta/onde-fica/onde-fica.component.ts
@@ -1,17 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { OfertasService } from 'src/app/ofertas.service';
 
 @Component({
   selector: 'app-onde-fica',
   templateUrl: './onde-fica.component.html',
-  styleUrls: ['./onde-fica.component.css']
+  styleUrls: ['./onde-fica.component.css'],
+  providers: [ OfertasService ]
 })
 export class OndeFicaComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) { }
+  public descricao: string = '';
+
+  constructor(
+    private route: ActivatedRoute,
+    private ofertasService: OfertasService) { }
 
   ngOnInit() {
-    console.log('ID da rota pai: ', this.route.parent.snapshot.params['id']);
+    this.ofertasService.getOndeFicaOfertaPorId(this.route.parent.snapshot.params['id'])
+      .then((descricao: string) => {
+        this.descricao = descricao;
+      })
   }
 
 }
