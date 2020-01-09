a310d93680bc1dcdb5b5fe873395e00eeb74abf0
Roneison M de Freitas <roneison.machado@gmail.com>
2020-01-09 09:34:33 -0300

    144. Incluindo o campo de pesquisa de ofertas

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -9,19 +9,19 @@ export class OfertasService {
     constructor(private http: HttpClient){}
 
     public getOfertas() : Promise<Oferta[]> {
-        return this.http.get(`${URL_API}/ofertas/?destaque=true`)
+        return this.http.get(`${URL_API}/ofertas?destaque=true`)
         .toPromise()
         .then((resposta : any) => resposta);
     }
 
     public getOfertasByCategoria(categoria: string) : Promise<Oferta[]> {
-        return this.http.get(`${URL_API}/ofertas/?categoria=${categoria}`)
+        return this.http.get(`${URL_API}/ofertas?categoria=${categoria}`)
         .toPromise()
         .then((resposta : any) => resposta);
     }
     
     public getOfertasById(id: number) : Promise<Oferta> {
-        return this.http.get(`${URL_API}/ofertas/?id=${id}`)
+        return this.http.get(`${URL_API}/ofertas?id=${id}`)
         .toPromise()
         .then((resposta: any) => {
             return resposta[0];
