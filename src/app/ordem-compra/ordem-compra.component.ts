67fff506cc985ec9b139f1cd52ddb00d666a6210
Roneison M de Freitas <roneison.machado@gmail.com>
2020-01-14 16:46:51 -0300

    177. Two-way-binding dos dados do formulario

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdemCompraComponent implements OnInit {
 
+  public endereco: string = '';
+  public numero: number;
+  public complemento: string = '';
+  public formaPagamento: string = '';
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public atualizaEndereco(endereco: string): void {
+    console.log('Endereco: ', endereco);
+    this.endereco = endereco;
+  }
+
+  public atualizaNumero(numero: number): void {
+    console.log('Numero: ', numero);
+    this.numero = numero;
+  }
+
+  public atualizaComplemento(complemento: string): void {
+    console.log('Complemento: ', complemento);
+    this.complemento = complemento;
+  }
+
+  public atualizaFormaPagamento(formaPagamento: string): void {
+    console.log('Forma de Pagamento: ', formaPagamento);
+    this.formaPagamento = formaPagamento;
   }
 
 }
