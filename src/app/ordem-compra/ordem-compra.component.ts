53b2bc166963fd12e6713cbe23565bffde5534c0
Roneison M de Freitas <roneison.machado@gmail.com>
2020-01-18 10:41:52 -0300

    207. Aplicando validacoes (Validators)

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { OrdemCompraService } from '../ordem-compra.service'
 import { Pedido } from '../shared/pedido.model'
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-ordem-compra',
@@ -12,10 +12,10 @@ import { FormGroup, FormControl } from '@angular/forms';
 export class OrdemCompraComponent implements OnInit {
 
   public formulario: FormGroup = new FormGroup({
-    'endereco': new FormControl(null),
-    'numero': new FormControl(null),
-    'complemento': new FormControl(null),
-    'formaPagamento': new FormControl(null)
+    'endereco': new FormControl(null, [ Validators.required, Validators.minLength(3), Validators.maxLength(60) ]),
+    'numero': new FormControl(null, [ Validators.required, Validators.maxLength(20) ]),
+    'complemento': new FormControl(null, [ Validators.maxLength(60) ]),
+    'formaPagamento': new FormControl(null, [ Validators.required ])
   });
 
   constructor(private ordemCompraService: OrdemCompraService) { }
