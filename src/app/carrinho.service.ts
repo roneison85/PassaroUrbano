a1ab0ab4f33dadcabf34ce8a6630ef5b6a9b7151
Roneison M de Freitas <roneison.machado@gmail.com>
2020-01-19 11:59:34 -0300

    214. Atribuindo CarrinhoService ao componente OrdemCompra

--- src/app/carrinho.service.ts
+++ src/app/carrinho.service.ts
@@ -1,5 +1,11 @@
+import { ItemCarrinho } from './shared/item-carrinho.model';
+
 class CarrinhoService {
-    public itens: CarrinhoService[] = [];
+    public itens: ItemCarrinho[] = [];
+
+    public exibirItens() : ItemCarrinho[] {
+        return this.itens;
+    }
 }
 
 export default CarrinhoService;
